d18e9238cadf37d2d99c0c70113174d2340035ac
FelipiFagundes <88801397+FelipiFagundes@users.noreply.github.com>
2021-08-19 13:30:56 -0300

    Add files via upload

---
+++ exercicio-video-aula08.js
@@ -0,0 +1,47 @@
+ /*
+ num = 0 
+do{
+  console.log(num++)
+}
+while (num <=10)
+
+for (num = 0; num <=5; num++)
+{
+  console.log(num)
+}
+*/
+
+
+alunos = ["João" , "Jose" , "Ana" , "Bia"]
+notas = [[7.5,8.7] , [4.8,5.7] , [9.5,6.4] , [8.6,3.5]]
+
+for (num = 0; num < alunos.length ; num ++) {
+
+selecionado = num
+resultado = ""
+
+nomeDoAluno = alunos[selecionado]
+notasDoAluno = notas[selecionado]
+
+nota1 = notasDoAluno[0]
+nota2 = notasDoAluno[1]
+
+media = (nota1 + nota2)/2
+
+ if (media >= 6.0){
+  resultado = "Parabéns!"
+  }
+
+  else {
+  resultado = "Tente mais uma vez!"
+  }
+
+
+
+console.log(nomeDoAluno + ",")
+console.log("Sua primeira nota foi " + nota1)
+console.log("Sua segunda nota foi " + nota2)
+console.log("Sua média foi " + media )
+console.log(resultado)
+console.log("")
+}
